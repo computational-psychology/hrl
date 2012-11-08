c7fd0a527850dc5a832c61cc891823f58046ca21
Sacha Sokoloski <sacha@cs.toronto.edu>
2012-11-09 00:17:20 +0100

    Reorganzing files in preperation for distutils.

--- docs/addmodeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cvt 1024 766 133
-xrandr --newmode "1024x766_133.00"  155.25  1024 1112 1216 1408  766 769 779 830 -hsync +vsync
-xrandr --addmode DVI1 1024x766_133.00
-xrandr --output DVI1 --mode 1024x766_133.00
-
-#xgamma -gamma 2.305
--- misc/setcpu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cpufreq-selector -c 0 -g performance
-cpufreq-selector -c 1 -g performance
