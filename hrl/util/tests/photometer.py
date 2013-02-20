5df36be6229db37f24e4ee2218cb843a5439b3f4
Sacha Sokoloski <sacha@cs.toronto.edu>
2013-02-20 14:49:11 +0100

    This time really with the test

---
+++ hrl/util/tests/photometer.py
@@ -0,0 +1,9 @@
+from hrl.photometer.optical import OptiCAL
+
+import numpy as np
+import pygame as pg
+import time
+
+
+phtm = OptiCAL('/dev/ttyUSB0')
+print('\nLuminance: ' + str(phtm.readLuminance(5,5)) + '\n')
