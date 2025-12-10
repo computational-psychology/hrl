4aae186deb22cc3f684b1a5c57fb9ad22c3e638d
Joris Vincent <joris.vincent@tu-berlin.de>
2025-12-10 17:15:15 +0100

    refactor(graphics.datapixx): update to match new organization

--- hrl/graphics/datapixx.py
+++ hrl/graphics/datapixx.py
@@ -1,4 +1,8 @@
+from functools import partial
+
 import numpy as np
+
+from hrl.luts import gamma_correct_grey
 
 from .graphics import Graphics
 
@@ -6,22 +10,51 @@ from .graphics import Graphics
 
 
 class DATAPixx(Graphics):
-    """
-    A Graphics implementation for a DataPixx DAC.  The four channel
-    representation has a 16 bit resolution in the  datapixx R-G concatenated
-    format.
-    """
+    """Graphics interface for 6-bit high-resolution greyscale for DataPixx/ViewPixx DACs"""
 
-    def greyToChannels(self, gry):
-        return dpxIntToChans(np.uint32(gry * (2**16 - 1)))
+    bitdepth = 8  # bit depth per physical channel
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-## Additional Functions ##
+        # Enable gamma correction
+        if self._lut is not None:
+            # TODO: validate LUT shape
+            self.gamma_correct = partial(gamma_correct_grey, LUT=self._lut)
 
+        # Here we change the default color
+        self.changeBackground(np.array(kwargs.get("background", 0.5)))
+        self.flip()
 
-def dpxIntToChans(n):
-    """
-    Takes a 16-bit integer and returns a 4-channel * 8-bit representation in the
-    datapixx R-G concatenated format.
-    """
-    return (n // (2**8), n % (2**8), 0, 2**8 - 1)
+    def channels_from_img(self, img):
+        """Convert greyscale image in [0.0, 1.0] to 4-channel, 8-bit integer representation
+
+        The four channel representation concatenates two 8-bit channels (R & G)
+        to form a 16-bit resolution, single-channel image.
+        This is the format required by the DataPixx DAC for high bit-depth greyscale output.
+
+        Parameters
+        ----------
+        img : Array[float]
+            Input greyscale image with values in [0.0, 1.0]
+
+        Returns
+        -------
+        Tuple[Array[uint8]]
+            4-channel representation as tuple of 8-bit integer arrays (R, G, B, Alpha)
+        """
+        assert img.ndim <= 2, "Input image must be single-channel HxW array"
+
+        # Discretize to 16-bit integers, single channel
+        arr = img * (2 ** (2 * self.bitdepth) - 1)
+        arr = np.uint32(arr)
+
+        # Convert to datapixx R-G concatenated format
+        channels = (
+            arr // (2**self.bitdepth),  # R channel (high byte)
+            arr % (2**self.bitdepth),  # G channel (low byte)
+            0,  # B channel (not used)
+            2**self.bitdepth - 1,  # Alpha channel (max intensity)
+        )
+
+        return channels
