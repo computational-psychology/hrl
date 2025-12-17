6c0f6a5a6a9a62f1c0bf809f2ebb23fe2fe3134a
Joris Vincent <joris.vincent@tu-berlin.de>
2025-12-17 13:55:38 +0100

    test: test the very basics: hrl, class, gpu and keyboard

---
+++ tests/test_basic.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+def test_import():
+    import hrl
+
+
+def test_gpu_init():
+    from hrl.graphics.gpu import GPU
+
+    igraphics = GPU(
+        width=200,
+        height=200,
+        background=0.5,
+        fullscreen=False,
+        double_buffer=True,
+        mouse=True,
+        lut=None,
+    )
+    igraphics.flip()
+
+
+def test_gpu_texture():
+    from hrl.graphics.gpu import GPU
+
+    igraphics = GPU(
+        width=200,
+        height=200,
+        background=0.5,
+        fullscreen=False,
+        double_buffer=True,
+        mouse=True,
+        lut=None,
+    )
+    igraphics.flip()
+    arr = np.random.random((100, 100))
+    itexture = igraphics.newTexture(arr)
+    itexture.draw(pos=(50, 50), sz=arr.shape)
+    igraphics.flip()
+
+
+def test_keyboard():
+    from hrl.inputs.keyboard import Keyboard
+
+    ikeyboard = Keyboard()
+    btns, _ = ikeyboard.readButton(btns=["Left", "Right"], to=0.01)
+    assert btns is None
+
+
+def test_hrl_class():
+    from hrl import HRL
+
+    ihrl = HRL(
+        graphics="gpu",
+        inputs="keyboard",
+        wdth=200,
+        hght=200,
+        bg=0.5,
+        fs=False,
+        db=True,
+        mouse=True,
+        lut=None,
+    )
+
+    # Test texture drawing
+    arr = np.random.random((100, 100))
+    itexture = ihrl.graphics.newTexture(arr)
+    itexture.draw(pos=(50, 50), sz=arr.shape)
+    ihrl.graphics.flip()
+
+    # Test keyboard input
+    btns, _ = ihrl.inputs.readButton(btns=["Left", "Right"], to=0.01)
+    assert btns is None
