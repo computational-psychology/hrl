c8dba8f5aebec0901952bac0bb0bad10f1055dc8
Joris Vincent <joris.vincent@tu-berlin.de>
2026-01-28 17:03:10 +0100

    refactor(graphics): remove outdated `channelsToInt`
    Not called anywhere, so not necessary.

--- hrl/graphics/graphics.py
+++ hrl/graphics/graphics.py
@@ -235,18 +235,3 @@ class Graphics(ABC):
         opengl.glClear(opengl.GL_COLOR_BUFFER_BIT)
 
         self.background = background
-
-
-def channelsToInt(t):
-    """
-    Takes a channel representation and returns a corresponding unsigned 32 bit
-    int.  Running the tostring method on a 2d array which has had this function
-    applied to it will produce a bytestring appropriate for use as a texture
-    with openGL.
-    """
-    r, g, b, a = t
-    R = 2**0
-    G = 2**8
-    B = 2**16
-    A = 2**24
-    return r * R + g * G + b * B + a * A
