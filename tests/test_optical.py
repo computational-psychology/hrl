c23ffeab1e05dd2fdf303e8788e97789633132d7
Joris Vincent <joris.vincent@tu-berlin.de>
2026-02-10 17:20:18 +0100

    test: OptiCAL as proper pytest
    No longer part of the hrl-util

---
+++ tests/test_optical.py
@@ -0,0 +1,9 @@
+from hrl.photometer.optical import OptiCAL
+
+
+def test_initialization():
+    OptiCAL(dev="/dev/ttyUSB0")
+
+
+def test_read_luminance():
+    OptiCAL(dev="/dev/ttyUSB0").readLuminance(n=5, slp=5)
