9feca3a55f6985ec0fdd541e47e7cc01d666ee8c
Joris Vincent <joris.vincent@tu-berlin.de>
2026-02-10 18:02:20 +0100

    test: import photometer modules only when necessary

--- tests/test_minolta.py
+++ tests/test_minolta.py
@@ -1,13 +1,15 @@
 import pytest
 
-from hrl.photometer.minolta import Minolta
-
 pytestmark = [pytest.mark.photometer]
 
 
 def test_initialization():
+    from hrl.photometer.minolta import Minolta
+
     Minolta(dev="/dev/ttyUSB0")
 
 
 def test_read_luminance():
+    from hrl.photometer.minolta import Minolta
+
     Minolta(dev="/dev/ttyUSB0").readLuminance(n=5, slp=5)
--- tests/test_optical.py
+++ tests/test_optical.py
@@ -1,13 +1,15 @@
 import pytest
 
-from hrl.photometer.optical import OptiCAL
-
 pytestmark = [pytest.mark.photometer]
 
 
 def test_initialization():
+    from hrl.photometer.optical import OptiCAL
+
     OptiCAL(dev="/dev/ttyUSB0")
 
 
 def test_read_luminance():
+    from hrl.photometer.optical import OptiCAL
+
     OptiCAL(dev="/dev/ttyUSB0").readLuminance(n=5, slp=5)
