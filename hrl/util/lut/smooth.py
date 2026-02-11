fix(util): only try to average if multiple measured values

hrl/util/lut/smooth.py
@@ -61,12 +61,13 @@ def smooth(args):
         # set outliers to NaN. Outliers are values that are more than 0.05 cd
         # or more than 0.5% of their value from the closest measurement at the
         # same intensity.
-        min_diff = np.empty_like(values)
-        for i in range(len(values)):
-            idx = np.ones(len(values), dtype=bool)
-            idx[i] = False
-            min_diff[i] = np.min(np.abs(values[idx] - values[i]))
-        values[(min_diff > 0.075) & (min_diff / values > 0.0075)] = np.nan
+        if len(values) > 1:
+            min_diff = np.empty_like(values)
+            for i in range(len(values)):
+                idx = np.ones(len(values), dtype=bool)
+                idx[i] = False
+                min_diff[i] = np.min(np.abs(values[idx] - values[i]))
+            values[(min_diff > 0.075) & (min_diff / values > 0.0075)] = np.nan
         hshmp[ky] = np.mean(values[np.isnan(values) == False])
         if np.isnan(hshmp[ky]):
             raise RuntimeError("no valid measurement for %f" % ky)