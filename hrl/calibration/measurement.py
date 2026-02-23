1e2e74eb33590cce34e2b9bf18f9971170a18d47
Joris Vincent <joris.vincent@tu-berlin.de>
2026-02-23 18:09:18 +0100

    feat(calibration): functions for combining, averaging, smooth (extracted)

--- hrl/calibration/measurement.py
+++ hrl/calibration/measurement.py
@@ -1,6 +1,71 @@
 import numpy as np
 
 
+def combine(measurements):
+    # Concatenate measurements into one big intensity to luminances map
+    luminance_map = {}
+    for table in measurements:
+        for row in table:
+            if row[0] in luminance_map:
+                luminance_map[row[0]] = np.concatenate([luminance_map[row[0]], row[1:]])
+            else:
+                luminance_map[row[0]] = row[1:]
+
+    return luminance_map
+
+
+def remove_outliers(luminance_map):
+    # Remove outliers and NaNs
+    # Outliers are values that are more than 0.05 cd or more than 0.5% of their value
+    # from the closest measurement at the same intensity.
+    for intensity, luminance_measurements in luminance_map.items():
+        luminance_measurements = luminance_measurements[~np.isnan(luminance_measurements)]
+
+        if len(luminance_measurements) > 1:
+            min_diff = np.empty_like(luminance_measurements)
+            for i in range(len(luminance_measurements)):
+                idx = np.ones(len(luminance_measurements), dtype=bool)
+                idx[i] = False
+                min_diff[i] = np.min(
+                    np.abs(luminance_measurements[idx] - luminance_measurements[i])
+                )
+            luminance_measurements[
+                (min_diff > 0.075) & (min_diff / luminance_measurements > 0.0075)
+            ] = np.nan
+
+        luminance_map[intensity] = luminance_measurements
+
+    return luminance_map
+
+
+def average(luminance_map):
+    # Average measurements at each intensity
+    for intensity, luminance_measurements in luminance_map.items():
+        luminance_map[intensity] = np.mean(
+            luminance_measurements[~np.isnan(luminance_measurements)]
+        )
+        if np.isnan(luminance_map[intensity]):
+            raise RuntimeError(f"no valid measurement for {intensity:.4f}")
+
+    table = np.array([list(luminance_map.keys()), list(luminance_map.values())]).transpose()
+    table = table[table[:, 0].argsort()]
+    # print(table.shape)
+
+    return table
+
+
+def smooth(luminances, order):
+
+    kernel = [0.2, 0.2, 0.2, 0.2, 0.2]
+
+    smoothed = luminances
+    for _ in range(order):
+        smoothed = np.hstack((np.ones(2) * smoothed[0], smoothed, np.ones(2) * smoothed[-1]))
+        smoothed = np.convolve(smoothed, kernel, "valid")
+
+    return smoothed
+
+
 def linearize(lut, bit_depth=16):
     """Sample a linear subset of the gamma table"""
     intensities = lut[:, 0]
