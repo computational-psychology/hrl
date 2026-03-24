c0e30a04d644b7fe7540d893769adb35d816c55e
Joris Vincent <joris.vincent@tu-berlin.de>
2026-03-24 16:27:55 +0100

    fix(util): remove markers in plot

--- hrl/util/lut/plot.py
+++ hrl/util/lut/plot.py
@@ -14,21 +14,21 @@ parser = argparse.ArgumentParser(
 
 
 def command(parsed_args):
-    lut = np.genfromtxt("lut.csv", skip_header=1, delimiter=',')
+    lut = np.genfromtxt("lut.csv", skip_header=1, delimiter=",")
 
     plt.figure()
 
     plt.subplot(211)
-    plt.plot(lut[:, 0], lut[:, 0], "x-", label="Original")
-    plt.plot(lut[:, 0], lut[:, 1], "o-", label="Corrected")
+    plt.plot(lut[:, 0], lut[:, 0], label="Original")
+    plt.plot(lut[:, 0], lut[:, 1], label="Corrected")
     plt.xlabel("Input Intensity")
     plt.ylabel("Output Intensity")
     plt.title("Corrected Intensity")
     plt.legend(loc="upper left")
 
     plt.subplot(212)
-    plt.plot(lut[:, 1], lut[:, 2], "x-", label="Original")
-    plt.plot(lut[:, 0], lut[:, 2], "o-", label="Corrected")
+    plt.plot(lut[:, 1], lut[:, 2], label="Original")
+    plt.plot(lut[:, 0], lut[:, 2], label="Corrected")
     plt.xlabel("Input Intensity")
     plt.ylabel("Luminance")
     plt.title("Corrected Luminance")
