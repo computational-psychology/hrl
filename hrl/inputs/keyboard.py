0c00af8d4d8d5a2c596f061fd0b90c04ea3604d4
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2026-06-22 13:28:16 +0200

    adds option of having digits as input

--- hrl/inputs/keyboard.py
+++ hrl/inputs/keyboard.py
@@ -53,5 +53,25 @@ def keyMap(ky):
         return "Space"
     elif ky == pygame.K_ESCAPE:
         return "Escape"
+    elif ky == pygame.K_0 or ky == pygame.K_KP0:
+        return "0"
+    elif ky == pygame.K_1 or ky == pygame.K_KP1:
+        return "1"
+    elif ky == pygame.K_2 or ky == pygame.K_KP2:
+        return "2"
+    elif ky == pygame.K_3 or ky == pygame.K_KP3:
+        return "3"
+    elif ky == pygame.K_4 or ky == pygame.K_KP4:
+        return "4"
+    elif ky == pygame.K_5 or ky == pygame.K_KP5:
+        return "5"
+    elif ky == pygame.K_6 or ky == pygame.K_KP6:
+        return "6"
+    elif ky == pygame.K_7 or ky == pygame.K_KP7:
+        return "7"
+    elif ky == pygame.K_8 or ky == pygame.K_KP8:
+        return "8"
+    elif ky == pygame.K_9 or ky == pygame.K_KP9:
+        return "9"
     else:
         return None
