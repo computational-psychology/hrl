813fdec9422f808c0c692622f9ee64e38580d5d9
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2026-07-15 20:20:23 +0200

    finishes implementation i1Pro

--- hrl/photometer/i1pro.py
+++ hrl/photometer/i1pro.py
@@ -1,14 +1,23 @@
-""" Class to read luminance values with x-rite i1Pro.
+""" Class to read luminance values with X-Rite i1Pro.
 
-It relies on the pypixxlib library, from VPixx 
+It relies on the pypixxlib library from VPixx 
 
 """
+
 from .photometer import Photometer
 from pypixxlib.i1 import I1Pro
 import pygame
 import numpy as np
 
-
+def wait_any_button(timeout=0):
+    t0 = pygame.time.get_ticks()
+    btn = None
+    while (timeout == 0) or (pygame.time.get_ticks() - t0 < timeout):
+        event = pygame.event.wait(1)  # waits for only 1 ms
+        if event.type == pygame.KEYDOWN:
+            break
+        
+        
 class i1Pro(Photometer):
     def __init__(self, timeout=5):
         super(i1Pro, self).__init__()
@@ -20,19 +29,29 @@ class i1Pro(Photometer):
      
         # check if calibration is necessary
         self.calibrate()
+                      
         
     def calibrate(self):
+        print('****************************************************')
         print("Calibrating device, put the device on its nest and push the side button")
         self.phtm.calibrate("Emission")
         
         print(f"Current color space is {self.phtm.getColorSpace()}")
         print(f"Current measurement mode is {self.phtm.getMeasurementMode()}")
         print(f"Current illumination mode is {self.phtm.getIlluminationMode()}")
-        print("... ready to measure.")
+        print("... Calibration done.")
+        print('****************************************************')
+        print('')
+        print('Put the device on the screen to be measured and press any key to start / continue the measurements')
+        wait_any_button(timeout=0)
 
-    def readLuminance(self, n=3, slp=1):
-        # check is calibration is necessary
+
+    def readTristimulus(self, n=3, slp=1, verbose=False):
         
+        # check if calibration is needed
+        if self.phtm.isCalibrationExpired():
+            self.calibrate()
+            
         # do measurements
         for i in range(n):
             try:
@@ -41,9 +60,25 @@ class i1Pro(Photometer):
                 # measure the XYZ tristimulus values
                 self.phtm.runMeasurement()
                 X, Y, Z = self.phtm.getLatestTriStimulusMeasurements()
-                print(f"X: {X}, Y: {Y}, Z:{Z}")                                         
-                return Y
+                if verbose:
+                    print('Tristimulus values:')
+                    print(f"X: {X}, Y: {Y}, Z:{Z}")
+                              
+                                         
+                return X, Y, Z
             except:
                 print("Error in reading from instrument")
         # if no try was successful
         return np.nan
+        
+        
+    def readLuminance(self, n=3, slp=1, verbose=False):
+        
+        # reads tristimulus values X, Y, Z.
+        _, lum, _ = self.readTristimulus(n=n, slp=slp, verbose=verbose)
+        
+        # returns Y, which is luminance by definition.    
+        return lum       
+
+        
+
