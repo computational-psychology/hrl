9d4bf1fdfd026ddbc43f2ea55d1bdb7b606ae340
Joris Vincent <joris.vincent@tu-berlin.de>
2026-07-28 20:39:03 +0200

    test: fix for when pypixxlib is not installed

--- tests/test_i1pro.py
+++ tests/test_i1pro.py
@@ -2,13 +2,14 @@ import pygame
 import pytest
 
 import hrl.photometer
-from hrl.photometer.i1pro import i1Pro
 
 pytestmark = [pytest.mark.photometer]
 pygame.init()
 
 
 def test_initialization():
+    from hrl.photometer.i1pro import i1Pro
+
     i1Pro(timeout=1)
 
 
@@ -17,13 +18,15 @@ def test_alias():
 
 
 def test_read_luminance():
-    device = i1Pro(timeout=1)
+    device = hrl.photometer.new_photometer("i1pro", timeout=1)
+
     lum = device.readLuminance(n=1, slp=5)
     assert isinstance(lum, float)
 
 
 def test_read_tristimulus():
-    device = i1Pro(timeout=1)
+    device = hrl.photometer.new_photometer("i1pro", timeout=1)
+
     xyz = device.readTristimulus(n=1, slp=5)
     assert isinstance(xyz, tuple)
     assert len(xyz) == 3
