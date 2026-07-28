5a83479bb8eabd0a0e6cb88db94bb64217c363dc
Joris Vincent <joris.vincent@tu-berlin.de>
2026-07-28 19:44:56 +0200

    test: assert output readLuminance

--- tests/test_i1pro.py
+++ tests/test_i1pro.py
@@ -19,3 +19,10 @@ def test_alias():
 def test_read_luminance():
     device = i1Pro(timeout=1)
     device.readLuminance(n=1, slp=5)
+
+
+def test_read_tristimulus():
+    device = i1Pro(timeout=1)
+    xyz = device.readTristimulus(n=1, slp=5)
+    assert isinstance(xyz, tuple)
+    assert len(xyz) == 3
